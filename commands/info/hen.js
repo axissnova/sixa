b1310f232235b1e289b1e0ce599814a904de9212
Colin Ciechon <itscolinciechon@gmail.com>
2020-03-30 20:57:48 -0400

    hen command poggers

---
+++ commands/info/hen.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "hen",
+    description: "returns with info",
+    category: "info",
+    run: async (client, message, args) => {
+        message.channel.send ("test")
+    }
+}
