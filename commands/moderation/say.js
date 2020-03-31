734607229afade7d8a59705c5bc9347f16cbdb80
Colin Ciechon <itscolinciechon@gmail.com>
2020-03-31 15:16:38 -0400

    removed line that didn't work

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -11,9 +11,6 @@ module.exports = {
         if (!message.member.hasPermission("MANAGE_MESSAGES"))
             return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
 
-        if (args.length < 0)
-            return message.reply("Nothing to say?").then(m => m.delete(5000));
-
         else {
             message.channel.send(args.join(" "));
         }
