7c7196da129db012404e5481094c00e60f1f52a1
Colin Ciechon <itscolinciechon@gmail.com>
2020-03-30 21:41:38 -0400

    removed embed ability cause code sucked

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -14,13 +14,7 @@ module.exports = {
         if (args.length < 0)
             return message.reply("Nothing to say?").then(m => m.delete(5000));
 
-        if (args[0].toLowerCase() === "embed") {
-            const embed = new RichEmbed()
-                .setDescription(args.slice(1).join(" "))
-                .setColor(roleColor === "#000000" ? "#ffffff" : roleColor);
-
-            message.channel.send(embed);
-        } else {
+        else {
             message.channel.send(args.join(" "));
         }
     }
