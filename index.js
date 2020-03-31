e7f9e3af5b26b8d63528fe1f3e9268ee24598cd9
Colin Ciechon <itscolinciechon@gmail.com>
2020-03-31 13:54:03 -0400

    edited prefix

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on("ready", () => {
 })
 
 client.on("message", async message => {
-    const prefix = "?";
+    const prefix = "$";
 
     if (message.author.bot) return;
     if (!message.guild) return;
