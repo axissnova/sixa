d932415df26f2e91e1ec8ea73bfa8f01e564f962
Colin Ciechon <itscolinciechon@gmail.com>
2020-03-31 22:12:10 -0400

    changed command name

---
+++ commands/info/furry.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: "furry",
+    description: "returns with info",
+    category: "info",
+    run: async (client, message, args) => {
+      
+      /*
+
+      const role = message.guild.roles.fetch(['684382675292913838']);
+
+      const member = message.mentions.members.first();
+
+      member.roles.add(role);
+      
+      */
+
+        /*
+        Note from Hen: Fuck you axiss
+        */
+    }
+}
