93c2c85d1e18313aae77fc4c814f61560a9c7117
Colin Ciechon <itscolinciechon@gmail.com>
2020-09-11 15:32:23 -0400

    say

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -8,11 +8,13 @@ module.exports = {
     run: (client, message, args) => {
         message.delete();
 
+        message.channel.send(args.join(" "));
+
         if (!message.member.hasPermission("ADMINISTRATOR"))
             return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
 
         else {
-            message.channel.send(args.join(" "));
-        }
+             message.channel.send(args.join(" "));
+         }
     }
 }
