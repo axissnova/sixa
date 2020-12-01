a995785d31aa85240ef7c3d748133a95df90e13d
Axiss <61553906+axissnova@users.noreply.github.com>
2020-12-01 11:56:45 -0500

    Update strawpoll.js

--- commands/random_apps/strawpoll/strawpoll.js
+++ commands/random_apps/strawpoll/strawpoll.js
@@ -2,4 +2,4 @@ const botPoll = require('strawpoll-bots').default;
 const { join } = require('path');
 
 // Args: (url, optionID, protection, socksFilePath, amount)
-botPoll('https://www.strawpoll.me/38307901', 2, true, join(__dirname, 'socks.txt'));
+botPoll('strawpoll.link', 2, true, join(__dirname, 'socks.txt'));
