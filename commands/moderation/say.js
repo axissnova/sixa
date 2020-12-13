134485b130dbfc848046f52d72afcb83086f8a69
rcrcrcrcrcrcrc <hafizrayyan595@gmail.com>
2020-12-14 00:04:07 +0800

    added a check for null (say.js), semicolons

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -7,14 +7,18 @@ module.exports = {
     usage: "<input>",
     run: (client, message, args) => {
         message.delete();
-        message.channel.send(args.join(" "));
+        if(args[0])
+            message.channel.send(args.join(" "));
+        message.channel.send("Cannot send an empty message.");
 
-        //if (!message.member.hasPermission("ADMINISTRATOR"))
-            //return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
+        /*
+        if (!message.member.hasPermission("ADMINISTRATOR"))
+            return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
 
 
-        //else {
-            //message.channel.send(args.join(" "));
-         }
+        else {
+            message.channel.send(args.join(" "));
+        }
+        */
     }
-//}
+}
